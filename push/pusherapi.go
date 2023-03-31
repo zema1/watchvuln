7719cf340f4fb71701356da8ec98d7c998e85bf3
lzskyline <lzskyline@foxmail.com>
2023-03-31 17:47:51 +0800

    [style] 代码质量优化.

--- push/pusherapi.go
+++ push/pusherapi.go
@@ -11,33 +11,37 @@ import (
 )
 
 type PusherAPI struct {
-	url string
-	log *golog.Logger
+	url    string
+	log    *golog.Logger
+	client *http.Client
 }
 
 type Params struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
+	Type    string `json:"type"`
 }
 
 func NewPusher(url string) *PusherAPI {
 	return &PusherAPI{
-		url: url,
-		log: golog.Child("pusherapi"),
+		url:    url,
+		log:    golog.Child("pusherapi"),
+		client: &http.Client{},
 	}
 }
 
 func (m *PusherAPI) PushText(s string) error {
 	params := &Params{
-		Title:   "程序异常",
 		Content: s,
+		Title:   "",
+		Type:    "text",
 	}
 
 	resp := m.postJSON(m.url, params)
 	if resp == nil {
 		return fmt.Errorf("failed to send text")
 	}
-	fmt.Println(string(resp))
+	m.log.Info(string(resp))
 	return nil
 }
 
@@ -45,42 +49,41 @@ func (m *PusherAPI) PushMarkdown(title, content string) error {
 	params := &Params{
 		Title:   title,
 		Content: content,
+		Type:    "markdown",
 	}
 
 	resp := m.postJSON(m.url, params)
 	if resp == nil {
 		return fmt.Errorf("failed to send text")
 	}
-	fmt.Println(string(resp))
+	m.log.Info(string(resp))
 	return nil
 }
 func (m *PusherAPI) postJSON(url string, params *Params) []byte {
 	postBody, _ := json.Marshal(params)
 
-	return doPostRequest(url, "application/json", postBody, m.log)
+	return m.doPostRequest(url, "application/json", postBody)
 }
 
-func doPostRequest(url string, contentType string, body []byte, log *golog.Logger) []byte {
-	client := &http.Client{}
-
+func (m *PusherAPI) doPostRequest(url string, contentType string, body []byte) []byte {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
-		log.Errorf("Error in creating request: %s", err)
+		m.log.Errorf("Error in creating request: %s", err)
 		return nil
 	}
 
 	req.Header.Set("Content-Type", contentType)
 
-	resp, err := client.Do(req)
+	resp, err := m.client.Do(req)
 	if err != nil {
-		log.Errorf("Error in sending request: %s", err)
+		m.log.Errorf("Error in sending request: %s", err)
 		return nil
 	}
 	defer resp.Body.Close()
 
 	respBody, err := io.ReadAll(resp.Body) // 使用 io.ReadAll 替代 ioutil.ReadAll。
 	if err != nil {
-		log.Errorf("Error in reading response body: %s", err)
+		m.log.Errorf("Error in reading response body: %s", err)
 		return nil
 	}
 
