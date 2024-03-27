103fe8fd67424854886b2a4df5fdb9e8ade52bee
Center-Sun <centersun61@gmail.com>
2024-03-27 21:37:05 +0800

    add grabber for cisa kev

---
+++ grab/kev_test.go
@@ -0,0 +1,29 @@
+package grab
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestKEV(t *testing.T) {
+	assert := require.New(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*300)
+	defer cancel()
+	grab := NewKEVCrawler()
+	vulns, err := grab.GetUpdate(ctx, 5)
+	assert.Nil(err)
+
+	count := 0
+	for _, v := range vulns {
+		t.Logf("get vuln info %s", v)
+		count++
+		assert.NotEmpty(v.UniqueKey)
+		assert.NotEmpty(v.Description)
+		assert.NotEmpty(v.Title)
+		assert.NotEmpty(v.Disclosure)
+
+	}
+	assert.Equal(count, 25)
+}
